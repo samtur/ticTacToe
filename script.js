49c8700203bab884922aac234a00f9e1d0f0f4b0
samtur <samturland23@gmail.com>
2022-12-16 22:31:16 +0000

    Set up objects and HTML

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ const gameBoard = (() => {
   return { board };
 })();
 
-console.log(gameBoard.board);
+const gameController = (() => {
+  let newGame = () => {};
+  let checkTurn = () => {};
+  let restartGame = () => {};
+  return { newGame, checkTurn, restartGame };
+})();
+
+const player = (name) => {
+  let playerName = name;
+  let playerScore = 0;
+  return { playerName, playerScore };
+};
