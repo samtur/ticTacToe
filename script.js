7e70b235e7d6be55169dac6f89ef60a9d96449c3
samtur <samturland23@gmail.com>
2022-12-19 22:36:39 +0000

    Added logic to check for a win

--- script.js
+++ script.js
@@ -10,21 +10,58 @@ const gameBoard = (() => {
       document.querySelector(`#sq${i}`).textContent = `${board[i]}`;
     }
   };
-  return { board, displayBoard };
+  const clearBoard = () => {};
+  return { board, displayBoard, clearBoard };
 })();
 
 const gameController = (() => {
   let checkWin = (board) => {
-    // WRITE LOGIC TO CHECK ALL WINNING POSSIBLITIES
+    const allX = (currentValue) => currentValue === "X";
+    const allO = (currentValue) => currentValue === "O";
     // END GAME WHEN WINNER CHECK IF IT IS PLAYER ONE OR PLAYER TWO
     // DISPLAY CONGRAULATIONS MESSAGE
+    // USE A DIV TO HOLD MESSAGE AT THE BOTTOM
     // UP PLAYERS SCORE
     // REMOVE ALL EVENT LISTENERS FROM BOARD
+    // REMOVE WINNER MESSAGE
     // ASK IF PLAYER WOULD LIKE ANOTHER ROUND
     // IF YES RUN NEWGAME FUNCTION AGAIN
+    // IF NO CURRENT A FUNCTION TO CLEAR EVERYTHING, BOARD AND SCORES
     // ADD OPTION FOR PLAYERS TO UPDATE NAME
-    if (board[0] === "X" && board[1] === "X" && board[2] === "X") {
-      console.log("Player One wins");
+    // ADD RESTART BUTTON AND FUNCTION WHICH RESTARTS CURRENT GAME
+    // LET NEW GAME BTN WIPE BOARD AND SCORES ON PRESS.
+    let check1 = [0, 1, 2].map((x) => board[x]);
+    let check2 = [3, 4, 5].map((x) => board[x]);
+    let check3 = [6, 7, 8].map((x) => board[x]);
+    let check4 = [2, 5, 8].map((x) => board[x]);
+    let check5 = [1, 4, 7].map((x) => board[x]);
+    let check6 = [0, 3, 6].map((x) => board[x]);
+    let check7 = [0, 4, 8].map((x) => board[x]);
+    let check8 = [6, 4, 2].map((x) => board[x]);
+    if (
+      check1.every(allX) ||
+      check2.every(allX) ||
+      check3.every(allX) ||
+      check4.every(allX) ||
+      check5.every(allX) ||
+      check6.every(allX) ||
+      check7.every(allX) ||
+      check8.every(allX)
+    ) {
+      console.log("player one winner");
+      return;
+    } else if (
+      check1.every(allO) ||
+      check2.every(allO) ||
+      check3.every(allO) ||
+      check4.every(allO) ||
+      check5.every(allO) ||
+      check6.every(allO) ||
+      check7.every(allO) ||
+      check8.every(allO)
+    ) {
+      console.log("player two winner");
+      return;
     }
     return;
   };
